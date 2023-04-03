2b0a3661848936051ea422768c7b9c671f681415
mohininimap <mohiniraut@nimapinfotech.com>
2023-04-03 19:32:07 +0530

    pair whose sum 100

--- index.js
+++ index.js
@@ -184,10 +184,20 @@ console.log(k)
 // Q.14. [80, 60, 10, 50, 30, 100, 0, 50]
 // Find pairs whose sum = 100;
 
-let arr3=[80, 60, 10, 50, 30, 100, 0, 50];
-let nu1;
-let nu2;
-
-for(let i=0;i<arr3.length;i++){
-console.log(arr3[i])
-}
+//WAP to find pairs whose sum is 100 from arr = [80,60,10,50,30,100,0,50]
+const findPair = (arr) => {
+    let pairs = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] + arr[j] === 100) {
+          pairs.push([arr[i], arr[j]]);
+        }
+      }
+    }
+    return pairs;
+  };
+  
+  const arr1 = [80, 60, 10, 50, 30, 100, 0, 50];
+  console.log(findPair(arr1));
+  
+  
